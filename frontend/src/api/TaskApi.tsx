9d88b8515313d59d9d3621cd67c07b1a8486e5be
utkuagus <utkuagus9@gmail.com>
2025-06-14 14:53:22 +0300

    online update is added

--- frontend/src/api/TaskApi.tsx
+++ frontend/src/api/TaskApi.tsx
@@ -70,3 +70,28 @@ export const revertTask = async (id: number | null = null): Promise<Task> => {
     throw err;
   }
 };
+
+export const updateTask = async (
+  id: number,
+  task: Partial<Task>
+): Promise<Task> => {
+  console.log("updating task: ", task);
+  try {
+    const response = await api.patch<Task>(URL + id + "/", task, {
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem("access")}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    const err = error as AxiosError;
+    if (err.response) {
+      console.error("Login failed:", err.response.data);
+    } else if (err.request) {
+      console.error("No response from server:", err.request);
+    } else {
+      console.error("Error setting up login request:", err.message);
+    }
+    throw err;
+  }
+};
--- frontend/src/components/Dashboard.tsx
+++ frontend/src/components/Dashboard.tsx
@@ -1,24 +1,45 @@
-import { useEffect, useState } from "react";
-import { completeTask, revertTask, getAllTasks } from "../api/TaskApi";
+import React, { useEffect, useState, useRef } from "react";
+import {
+  completeTask,
+  revertTask,
+  getAllTasks,
+  updateTask,
+} from "../api/TaskApi";
 import type { GetAllTasksResponse } from "../types/ApiDTO";
 import "../css/Dashboard.css";
+import type { Task } from "../types/Models";
 
 interface props {
   token: string;
 }
 
 const Dashboard: React.FC<props> = ({ token }) => {
-  const [taskList, setTaskList] = useState<GetAllTasksResponse>();
-  const [error, setError] = useState<String>();
-  const [isUpdatedTrigger, setIsUpdatedTrigger] = useState<Boolean>(false);
+  const [isUpdatedTrigger, setIsUpdatedTrigger] = useState<boolean>(false);
+  const [updatingRowId, setUpdatingRowId] = useState<number | null>(null);
+  const [isUpdatingTableCompleted, setIsUpdatingTableCompleted] =
+    useState<boolean>(false);
+  const [tempTask, setTempTask] = useState<Task | null>(null);
+
+  const [todoList, setTodoList] = useState<GetAllTasksResponse>([]);
+  const [completedList, setCompletedList] = useState<GetAllTasksResponse>([]);
+
+  const taskList: Record<string, Task[]> = {
+    false: todoList,
+    true: completedList,
+  };
+
+  const dueDateRef = useRef<HTMLInputElement>(null);
+  const updateButtonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
     const get = async () => {
       try {
         const resp = await getAllTasks();
-        setTaskList(resp);
+        setTodoList(resp.filter((task) => !task.is_completed));
+        setCompletedList(resp.filter((task) => task.is_completed));
+        setUpdatingRowId(null);
       } catch (err) {
-        setError("Task list could not be retrieved.");
+        console.log("Task list could not be retrieved.");
       }
     };
 
@@ -31,12 +52,16 @@ const Dashboard: React.FC<props> = ({ token }) => {
     get();
   }, [token, isUpdatedTrigger]);
 
+  useEffect(() => {
+    setTempTask(null);
+  }, [updatingRowId]);
+
   const handleComplete = async (id: number) => {
     try {
       await completeTask(id);
       setIsUpdatedTrigger((ut) => !ut);
     } catch (err) {
-      setError(`Task id = ${id} could not be completed`);
+      console.log(`Task id = ${id} could not be completed`);
     }
   };
 
@@ -45,7 +70,44 @@ const Dashboard: React.FC<props> = ({ token }) => {
       await revertTask(id);
       setIsUpdatedTrigger((ut) => !ut);
     } catch (err) {
-      setError(`Task id = ${id} could not be completed`);
+      console.log(`Task id = ${id} could not be completed`);
+    }
+  };
+
+  const handleInputChange = async <K extends keyof Task>(
+    e: React.ChangeEvent<HTMLInputElement>,
+    paramName: K
+  ) => {
+    const value = e.target.value;
+    const task: Partial<Task> = {
+      [paramName]: value,
+    };
+    const origTask = tempTask
+      ? tempTask
+      : taskList[isUpdatingTableCompleted.toString()][updatingRowId!];
+    setTempTask({ ...origTask, ...task });
+  };
+
+  const handleUpdate = async (id: number) => {
+    try {
+      if (tempTask) {
+        await updateTask(id, tempTask);
+      }
+    } catch (err) {
+      console.log(`Task id = ${id} could not be updated`);
+    }
+    setIsUpdatedTrigger((ut) => !ut);
+  };
+
+  const handleTitleEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      dueDateRef.current?.focus();
+    }
+  };
+
+  const handleDueDateEnter = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      updateButtonRef.current?.click();
     }
   };
 
@@ -53,19 +115,65 @@ const Dashboard: React.FC<props> = ({ token }) => {
 
   const getTaskTable = (
     taskList: GetAllTasksResponse,
-    title: String,
-    buttonName: String,
-    handleFunc: (id: number) => void
+    title: string,
+    buttonName: string,
+    handleFunc: (id: number) => void,
+    isCompleted: boolean
   ) => {
     return (
       <div className="task-table flex column center-y">
         <h3>{title}</h3>
         <div className="tasks flex column gap-10">
-          {taskList.map((task) => (
-            <div className="task-row flex space-between center-y" key={task.id}>
-              <div className="title">{task.title}</div>
-              <div className="due_date">{task.due_date}</div>
-              <button onClick={(e) => handleFunc(task.id)}>{buttonName}</button>
+          {taskList.map((task, idx) => (
+            <div
+              className="task-row flex space-between center-y"
+              key={task.id}
+              onClick={() => {
+                setUpdatingRowId(idx);
+                setIsUpdatingTableCompleted(isCompleted);
+              }}
+            >
+              {updatingRowId == idx &&
+              isUpdatingTableCompleted == isCompleted ? (
+                <>
+                  <input
+                    className="title"
+                    value={tempTask ? tempTask.title : task.title}
+                    onChange={(e) => handleInputChange(e, "title")}
+                    onKeyDown={handleTitleEnter}
+                  />{" "}
+                  <input
+                    type="date"
+                    className="due_date"
+                    ref={dueDateRef}
+                    value={tempTask ? tempTask.due_date : task.due_date}
+                    onChange={(e) => handleInputChange(e, "due_date")}
+                    onKeyDown={handleDueDateEnter}
+                  />
+                  <button
+                    ref={updateButtonRef}
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleUpdate(task.id);
+                    }}
+                  >
+                    update task
+                  </button>
+                </>
+              ) : (
+                <>
+                  <div className="title">{task.title}</div>
+                  <div className="due_date">{task.due_date}</div>{" "}
+                  <button
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      handleFunc(task.id);
+                    }}
+                  >
+                    {buttonName}
+                  </button>
+                </>
+              )}
             </div>
           ))}
         </div>
@@ -77,17 +185,13 @@ const Dashboard: React.FC<props> = ({ token }) => {
     <div className="dashboard-container">
       <h2>Dashboard</h2>
       <div className="task-categories flex center-x">
+        {getTaskTable(todoList, "Todo List", "complete", handleComplete, false)}
         {getTaskTable(
-          taskList.filter((task) => !task.is_completed),
-          "Todo List",
-          "complete",
-          handleComplete
-        )}
-        {getTaskTable(
-          taskList.filter((task) => task.is_completed),
+          completedList,
           "Completed List",
           "revert",
-          handleRevert
+          handleRevert,
+          true
         )}
       </div>
     </div>
